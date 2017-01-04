012a6d5dc5df3d733bf87378bcc3fd26100bac0b
sbirdsall0312 <sbirdsall0312@gmail.com>
2017-01-03 19:41:23 -0500

    Fixed about-img margins

--- public/js/controller.main.js
+++ public/js/controller.main.js
@@ -4,6 +4,18 @@ angular.module('awp')
 function mainController() {
   var main = this;
   
+  main.volData = {};
   
+  main.formPage = 1;
+  
+  main.nextBtn = function(){
+    main.formPage++;
+    console.log(main.volData);
+  }
+  
+  main.prevBtn = function(){
+    main.formPage--;
+    console.log(main.volData);
+  }
   
 }
