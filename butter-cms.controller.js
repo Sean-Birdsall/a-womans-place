a0a6c5abbb20712a83bb9ee8194225550bd7ee26
sbirdsall0312 <sbirdsall0312@gmail.com>
2017-02-22 14:28:20 -0500

    AWP updates feb22

--- butter-cms.controller.js
+++ butter-cms.controller.js
@@ -3,8 +3,9 @@ var butter = require('buttercms')(lame.APIkey);
 
 
 var contentFields = ['image_carousel','home_page_banner','home_sections',
-'about_page_images','about_page_banner', 'about_page_sections', 'board_of_directors', 'executive_director',
-'services', 'events_banner_image','events', 'wishlist', 'vol_positions'];
+'about_page_banner', 'about_page_sections', 'board_of_directors', 'executive_director',
+'services','creating_a_safety_plan','preparing_to_leave', 'get_involved', 'get_involved_image', 'events_banner_image','events', 
+ 'wishlist', 'vol_positions'];
 
 module.exports = {
 
