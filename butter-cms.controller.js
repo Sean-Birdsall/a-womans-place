a8ea9933e722c0ea775fa3923ba6165fe62459db
Kirsten Ketner <runningwithscissors09@gmail.com>
2017-04-27 22:32:29 -0600

    Trying to format new sections content correctly. Lorem ipsum filler for dynamics section.

--- butter-cms.controller.js
+++ butter-cms.controller.js
@@ -7,7 +7,7 @@ var contentFields = ['image_carousel','home_page_banner','home_sections',
 'services','creating_a_safety_plan','preparing_to_leave', 'protecting_online_privacy',
 'community_resources', 'what_is_dv', 'warning_signs', 'obstacles',
 'get_involved', 'get_involved_image', 'events_banner_image', 'events', 'wishlist',
-'vol_positions'];
+'vol_positions', 'affects_children'];
 
 module.exports = {
 
