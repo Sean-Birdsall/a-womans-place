ee1c854d3e69767a01295e85df8d1f5621828066
Aaron Pennington <aaron.l.pennington@gmail.com>
2017-05-04 15:01:39 -0600

    set up new resources page to pull data from butterCMS api as per issue 24

--- public/js/controller.main.js
+++ public/js/controller.main.js
@@ -1,6 +1,7 @@
 angular.module('awp')
   .controller('mainController', mainController);
 
+
 mainController.$inject = ['$http', '$location', '$sce'];
 
 function mainController($http, $location, $sce) {
@@ -46,9 +47,18 @@ function mainController($http, $location, $sce) {
 
 
       // Main object of CMS response
-      main.awp = res.data.data;
+  main.awp = res.data.data;
+    console.log(main.awp);
 
-            console.log(main.awp.obstacles);
+    // the butter-cms response sends sends many description values as HTML text.
+    // the butter-cms gives the option of having a text area set as a WYSIWYG editor, which formats all in it as HTML.
+    // This is how (one way?) to make the HTML in those discriptions acceptable to display correctly in angularJS.
+    // They have to be set as safe...
+  main.communityResources = $sce.trustAsHtml(main.awp.community_resources[0].description);
+  main.prepToLeave        = $sce.trustAsHtml(main.awp.preparing_to_leave[0].description);
+  main.safetyPlan         = $sce.trustAsHtml(main.awp.creating_a_safety_plan[0].description);
+  main.onlinePrivacy      = $sce.trustAsHtml(main.awp.protecting_online_privacy[0].description);
+      // console.log(main.awp.obstacles);
 
       /////////////////////// HOME PAGE DATA //////////////////////////////////
       // Data used in bootstrap carousel
@@ -65,19 +75,46 @@ function mainController($http, $location, $sce) {
       main.rightServices = res.data.data.services.slice(Math.ceil(res.data.data.services.length/2));
       main.leftServices = res.data.data.services.splice(0, Math.ceil(res.data.data.services.length/2));
 
-      main.getHelpData = function(){
-        setTimeout(function(){
-          var prepToLeave = $('#prep-to-leave');
-          var safetyPlan = $('#safety-plan');
-          var onlinePrivacy = $('#online-privacy');
-          //var communityRes = $('#community-res');
 
-          prepToLeave[0].innerHTML = main.awp.preparing_to_leave[0].description;
-          safetyPlan[0].innerHTML = main.awp.creating_a_safety_plan[0].description;
-          onlinePrivacy[0].innerHTML = main.awp.protecting_online_privacy[0].description;
-          //communityRes[0].innerHTML = main.awp.community_resources[0].description;
-        }, 500);
-      }
+      /////////////////////// RESOURCES PAGE DATA //////////////////////////////
+
+
+      main.resources = res.data.data.resources;
+
+
+
+// <<<<<<< HEAD
+//
+//       //======================FUNCTION MAKE CALL FOR GET HELP DATA===============
+//       main.getHelpData = function(){
+//         setTimeout(function(){
+//           var prepToLeave = $('#prep-to-leave');
+//           var safetyPlan = $('#safety-plan');
+//           var onlinePrivacy = $('#online-privacy');
+//           // var communityRes = $('#community-res');
+//
+//           // console.log("GET HELP: ", prepToLeave, safetyPlan, onlinePrivacy);
+//
+//           prepToLeave[0].innerHTML = main.awp.preparing_to_leave[0].description;
+//           safetyPlan[0].innerHTML = main.awp.creating_a_safety_plan[0].description;
+//           onlinePrivacy[0].innerHTML = main.awp.protecting_online_privacy[0].description;
+//           // communityRes[0].innerHTML = main.awp.community_resources[0].description;
+//         }, 1000);
+//       }
+
+// I do not understand why this function was chosen to display the relevant data...
+      // main.getHelpData = function(){
+      //   setTimeout(function(){
+      //     var prepToLeave = $('#prep-to-leave');
+      //     var safetyPlan = $('#safety-plan');
+      //     var onlinePrivacy = $('#online-privacy');
+      //     var communityRes = $('#community-res');
+      //
+          // prepToLeave[0].innerHTML = main.awp.preparing_to_leave[0].description;
+          // communityRes[0].innerHTML = main.awp.community_resources[0].description;
+//       //   }, 500);
+//       // }
+// >>>>>>> 276dac98be918d60340e0f60d2b838b0748ff12a
 
       if ($location.$$absUrl.slice(17) == '#/getHelp'){
         main.getHelpData();
@@ -219,19 +256,26 @@ function mainController($http, $location, $sce) {
     main.formPage--;
   }
 
+
 // Data for the resources links. Just replace resource names and urls with your own data. Cut and paste last object if you need more reqources. Delete objects if you need less.
-  main.resources = [{
-    name: "resource 1",
-    url: "https://www.google.com"
-  },{
-    name: "resource 2",
-    url: "https://www.google.ca"
-  },{
-    name: "resource 3",
-    url: "https://www.google.com.mx"
-  },{
-    name: "resource 4",
-    url: "https://www.google.com.au"
-  }]
+
+  // main.resources = main.awp.resources;
+
+  // main.resources = [{
+  //   name: "resource 1",
+  //   url: "https://www.google.com"
+  // },{
+  //   name: "resource 2",
+  //   url: "https://www.google.ca"
+  // },{
+  //   name: "resource 3",
+  //   url: "https://www.google.com.mx"
+  // },{
+  //   name: "resource 4",
+  //   url: "https://www.google.com.au"
+  // }]
+
+
+  console.log(main.resources);
 
 }
