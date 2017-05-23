49b69844c2026ab952231a83f67fc5c43ad67780
Edward McCarthy <efmccarthy26@gmail.com>
2017-05-23 07:49:35 -0600

    test script in place and first test passing

--- test/server.test.js
+++ test/server.test.js
@@ -1,1 +1,11 @@
 const request = require("supertest");
+
+// var app = require("./server").app;
+var app = require("../server").app;
+
+it("should return response", (done) => {
+  request(app)
+    .get("/")
+    .expect("Content-Type", /html/)
+    .end(done);
+});
