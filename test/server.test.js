8e4fa39f26a3a37baa4bd24aed71d9e588adc9c6
Edward McCarthy <efmccarthy26@gmail.com>
2017-05-26 16:29:18 -0600

    Initial unit testing is passing for Home page and About. Looking at deeper testing methods for the other pages

--- test/server.test.js
+++ test/server.test.js
@@ -1,11 +1,32 @@
 const request = require("supertest");
+const expect = require("expect");
 
-// var app = require("./server").app;
 var app = require("../server").app;
 
-it("should return response", (done) => {
-  request(app)
-    .get("/")
-    .expect("Content-Type", /html/)
-    .end(done);
+describe('loading express server', function () {
+
+  it("should respond with the home page", (done) => {
+    request(app)
+      .get("/")
+      .expect(200)
+      .expect("Content-Type", /html/)
+      .end(done);
+  });
+
+  it("404s bad addresses", (done) => {
+    request(app)
+      .get("/foo/bar")
+      .expect(404)
+      .end(done);
+  });
+
+  describe("getting to about page", function() {
+    it("get's a 200", (done) => {
+      request(app)
+        .get("/#/about")
+        .expect(200)
+        .end(done);
+    });
+  });
+
 });
